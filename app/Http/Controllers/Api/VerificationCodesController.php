8b6e1e99fd4445178f35934445fb5622605799a8
Your Name <you@example.com>
2018-11-13 07:55:24 +0000

    verificationCode store

---
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Overtrue\EasySms\EasySms;
+use App\Http\Requests\Api\VerificationCodeRequest;
+
+class VerificationCodesController extends Controller
+{
+    public function store(VerificationCodeRequest $request, EasySms $easySms)
+    {
+        $phone = $request->phone;
+
+        
+        if (!app()->environment('production')) 
+        {
+   	 		$code = '1234';
+		}
+        else
+        {
+        	// 生成4位随机数，左侧补0
+       		 $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+       		 
+        	try {
+	            $result = $easySms->send($phone, [
+	                'content'  => '【ISMTA实验室】温馨提示，您有一个关于官网-关于我们-进群的实验室任务未完成，如需帮助请及时与实验室管理员联系，谢谢^_^',
+	            ]);
+	        } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+	            $message = $exception->getException('yunpian')->getMessage();
+	            return $this->response->errorInternal($message ?? '短信发送异常');
+	        }
+        }
+	        
+
+        $key = 'verificationCode_'.str_random(15);
+        $expiredAt = now()->addMinutes(10);
+        // 缓存验证码 10分钟过期。
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        return $this->response->array([
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
+    }
+}
