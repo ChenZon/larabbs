82d1e2ad70b71c30e72af092fa4f526bb87e01f7
Your Name <you@example.com>
2018-11-14 09:46:20 +0000

    api/user

--- routes/api.php
+++ routes/api.php
@@ -4,22 +4,21 @@ use Illuminate\Http\Request;
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api',
-], function($api) {
+$api->version('v1', ['namespace' => 'App\Http\Controllers\Api','middleware' => 'serializer:array'], function ($api) 
+{
 
-     $api->group([
-        'middleware' => 'api.throttle',
+     $api->group(['middleware' => 'api.throttle',
         'limit' => config('api.rate_limits.sign.limit'),
         'expires' => config('api.rate_limits.sign.expires'),
-    ], function($api) {
+    ], function($api)
+    {
         // 短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store')
             ->name('api.verificationCodes.store');
         // 用户注册
-		$api->post('users', 'UsersController@store')
-		    ->name('api.users.store');
-		// 图片验证码
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+        // 图片验证码
         $api->post('captchas', 'CaptchasController@store')
             ->name('api.captchas.store');
         // 第三方登录
@@ -34,9 +33,56 @@ $api->version('v1', [
             ->name('api.authorizations.update');
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
-            ->name('api.authorizations.destroy');
-
-        
-            
+            ->name('api.authorizations.destroy');                   
     });
+
+
+      $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
+    ], function ($api) 
+    {
+        // 游客可以访问的接口
+
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
+    });
+
+      // 需要 token 验证的接口
+    $api->group(['middleware' => 'api.auth'], function($api) 
+    {
+        // 当前登录用户信息
+        $api->get('user', 'UsersController@me')
+            ->name('api.user.show');
+        // 图片资源
+        $api->post('images', 'ImagesController@store')
+            ->name('api.images.store');
+    });
+
+
+
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
+    ], function ($api) 
+    {
+        // 游客可以访问的接口
+
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api)
+         {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
+    });
+
+
+    
 });
