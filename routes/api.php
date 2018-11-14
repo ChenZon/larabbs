282e38933d8e5842c1ffaadbad9b3b9d5eef97fb
Your Name <you@example.com>
2018-11-14 10:10:54 +0000

    user update

--- routes/api.php
+++ routes/api.php
@@ -59,6 +59,9 @@ $api->version('v1', ['namespace' => 'App\Http\Controllers\Api','middleware' => '
         // 当前登录用户信息
         $api->get('user', 'UsersController@me')
             ->name('api.user.show');
+        // 编辑登录用户信息
+        $api->patch('user', 'UsersController@update')
+            ->name('api.user.update');
         // 图片资源
         $api->post('images', 'ImagesController@store')
             ->name('api.images.store');
@@ -82,6 +85,25 @@ $api->version('v1', ['namespace' => 'App\Http\Controllers\Api','middleware' => '
                 ->name('api.user.show');
         });
     });
+
+    // 需要 token 验证的接口
+    $api->group(['middleware' => 'api.auth'], function($api) {
+        // 当前登录用户信息
+        $api->get('user', 'UsersController@me')
+            ->name('api.user.show');
+        // 图片资源
+        $api->post('images', 'ImagesController@store')
+            ->name('api.images.store');
+    });
+
+
+
+
+
+
+
+
+
 
 
     
